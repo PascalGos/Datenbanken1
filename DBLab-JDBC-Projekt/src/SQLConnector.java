9631f696ab71f754870c7012e74c07d89b7eabaf
Sebastian Linke <lise1011@IWI-lkit3-01>
2017-12-04 15:49:27 +0100

    Treiber eingebunden

--- DBLab-JDBC-Projekt/src/SQLConnector.java
+++ DBLab-JDBC-Projekt/src/SQLConnector.java
@@ -119,21 +119,9 @@ public abstract class SQLConnector {
      * (siehe auch Dokumentation des Datenbanktreibers)
      *  - Erstellt eine Verbindung mit Hilfe der Klassenvariablen.
      */
-    protected Connection getConnection() throws SQLException{
+    protected Connection getConnection() {
         // TODO begin
-    	try{
-    		Class.forName(driverClassName);
-    		Connection conn = DriverManager.getConnection(databaseURL, user, password);
-    		return conn;
-    	}
-    	catch(SQLException e){
-    		System.out.println(e);
-    		//e.printStackTrace();
-    	}
-    	catch(ClassNotFoundException e){
-    		System.out.println(e);
-    	}
-    	return null;
+        return null;
         // TODO end
     }
 
@@ -167,12 +155,10 @@ public abstract class SQLConnector {
         if (null != connection && !connection.isClosed()) {
             System.out.println("Connection successfully established.");
         } else {
-            System.err.println("\nEstablishing the connection failed!");
+            System.err.println("Establishing the connection failed!");
         }
-        
-        if(connection!=null){
-        	connection.close();
-        }
+
+        connection.close();
     }
 }
 
@@ -188,8 +174,8 @@ class SQLConnectorOracleHsInternal extends SQLConnector{
 	protected SQLConnectorOracleHsInternal() {
 		// Bitte auf Ihre Gruppe anpassen ...
 		this.databaseURL =     "jdbc:oracle:thin:@iwi-lkit-db-01:1521:LAB1";
-		this.user =            "dbprax25";  // TODO für Ihre Gruppe anpassen
-		this.password =        "fuckyou1";  // TODO für Ihre Gruppe anpassen
+		this.user =            "dbpraxNn";  // TODO für Ihre Gruppe anpassen
+		this.password =        "dbpraxNn";  // TODO für Ihre Gruppe anpassen
 		this.driverClassName = "oracle.jdbc.OracleDriver";
 	}
 }
